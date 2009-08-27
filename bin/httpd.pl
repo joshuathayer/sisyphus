1d28cb2488b99e2af4c716284a1233291a96c2a0
Joshua Thayer <joshuamilesthayer@gmail.com>
2009-08-26 23:59:45 -0700

    AnyEvent::HTTPD changed significantly... changing sis's HTTP module to catch up.

--- bin/httpd.pl
+++ bin/httpd.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/perl
 
 use strict;
-use lib ('/home/joshua/projects/sisyphus/lib');
+use lib ('/home/joshua/sisyphus/lib');
+use lib ('/home/joshua/sisyphus/req/EV-3.8');
+use lib ('/home/joshua/sisyphus/req/AnyEvent-5.112/lib');
+use lib ('/home/joshua/sisyphus/req/MySQL-Packet-0.2007054/lib');
+use lib ('/home/joshua/sisyphus/req/AnyEvent-HTTP-1.43');
+use lib ('/home/joshua/sisyphus/req/AnyEvent-HTTPD-0.82/lib');
+use lib ('/home/joshua/sisyphus/req/common-sense-1.0/blib/lib');
+use lib ('/home/joshua/sisyphus/req/Object-Event-1.1/lib');
+
 
 use Sisyphus::Listener;
 use Sisyphus::Proto::HTTP;
@@ -11,7 +19,7 @@ use ExampleHTTPDApplication;
 my $listener = new Sisyphus::Listener;
 
 $listener->{port} = 8889;
-$listener->{ip} = "192.168.170.216";
+$listener->{ip} = "172.16.5.9";
 $listener->{protocol} = "Sisyphus::Proto::HTTP";
 $listener->{application} = ExampleHTTPDApplication->new();
 $listener->{use_push_write} = 0;
--- lib/Sisyphus/Proto/HTTP.pm
+++ lib/Sisyphus/Proto/HTTP.pm
@@ -1,46 +1,66 @@
 package Sisyphus::Proto::HTTP;
+use strict;
+use base 'AnyEvent::HTTPD::HTTPConnection';
 
-use AnyEvent::HTTPD;
-use AnyEvent::HTTPD::HTTPConnection;
 use Data::Dumper;
 use Date::Format;
 
+# this is basically a shim between the Sisyphus framework and
+# AnyEvent::HTTPD::HTTPConnection. This class inherits from
+# AE:H:HTTPConnection in order to avoid calling that class's 
+# constructor, which tries to take control by creating its own
+# Handle object (which, in the Sisyphus universe, is done in the
+# Listener.
+
 sub new {
-	my $this = shift;
+	my $this  = shift;
 	my $class = ref($this) || $this;
 	my $self  = { @_ };
+	print "class $class\n";
+	bless $self, $class;
 
-	$self->{HTTPConnection} = new AnyEvent::HTTPD::HTTPConnection;
+	$self->{request_timeout} = 60
+	unless defined $self->{request_timeout};
 
-	# A::H::HTTPConnection uses the Object::Event framework, which is rather neat
-	$self->{HTTPConnection}->reg_cb(request => sub {
+	return $self
+}
+
+# called by framework when a client connects.
+# a bunch of the things done in AE::H::HTTPConnection's
+# constructor are done here instead (register the "request"
+# callback, assign the handle)
+sub on_client_connect {
+	my $self = shift;
+
+	print "hello from on_client_connect\n";
+
+	$self->reg_cb(request => sub {
 		# stolen from A::HTTPD.pm's new():
 		my ($con, $meth, $url, $hdr, $cont) = @_;
 		$self->request($con, $meth, $url, $hdr, $cont);
 	});
 
-	bless $self, $class;
+	# at this point, $self->{handle} has been set to a new AE::Handle object. 
+	# AE::HTTPD::HTTPConnection (from which this class descends) expects that
+	# as $self->{hdl}. so...
+	$self->{hdl} = $self->{handle};
+
+	# now, we let AE::HTTPD::HTTPConnetion run the show
+	$self->push_header_line;
 }
 
-# called by framework when a client connects.
-sub on_client_connect {
-	my $self = shift;
-	print "hello from on_client_connect\n";
-	$self->consume();
-}
-
-# just pull bytes off the wire and give them to the httpd instance
-sub consume {
-	my $self = shift;
-	my $handle = $self->{handle};
-	print "consume!\n";
-	$handle->on_read(sub {
-		#my ($handle, $in) = @_;
-		my $handle = shift;
-		print "in "  . $handle->{rbuf} ."\n";
-		$self->{HTTPConnection}->handle_data(\$handle->{rbuf});
-	});
-}
+## just pull bytes off the wire and give them to the httpd instance
+#sub consume {
+#	my $self = shift;
+#	my $handle = $self->{handle};
+#	print "consume!\n";
+#	$handle->on_read(sub {
+#		#my ($handle, $in) = @_;
+#		my $handle = shift;
+#		print "in "  . $handle->{rbuf} ."\n";
+#		$self->{HTTPConnection}->handle_data(\$handle->{rbuf});
+#	});
+#}
 
 sub request {
 	# called by A::H::HTTPConnection on request
@@ -48,10 +68,10 @@ sub request {
 	my ($self, $con, $meth, $url, $hdr, $cont)  = @_;
 
 	print "in HTTP::request\n";
-	print Dumper $con;
-	print Dumper $meth;
-	print Dumper $url;
-	print Dumper $hdr;
+	#print Dumper $con;
+	#print Dumper $meth;
+	#print Dumper $url;
+	#print Dumper $hdr;
 
 	$self->{app_callback}->($meth, $url, $hdr, $cont);
 }
